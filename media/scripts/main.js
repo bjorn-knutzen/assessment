b73e7de11d96449165908a3502e503f3c6319cf0
bjorn-knutzen <62984016+bjorn-knutzen@users.noreply.github.com>
2022-09-12 00:14:35 +0200

    Add search bar

--- media/scripts/main.js
+++ media/scripts/main.js
@@ -13,8 +13,45 @@ $(window).scroll(function() {
 $(document).ready(function(){
     $('#nav-button').click(function(){
         $(this).toggleClass('nav--open');
-        $(".nav__items").toggleClass("nav__items__open");
+        $(".nav__items").toggleClass("nav__items--open");
     });
 });
 
+//CARROUSEL
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+//Carrousel behaviour
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slideshow__item");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+//SEARCH FIELD
+
+$(document).ready(function(){
+    $('.search__active-field').click(function(){
+        $(this).toggleClass('search__field-holder--active');
+        $(".search__field-holder").toggleClass("search__field-holder--active");
+    });
+});
